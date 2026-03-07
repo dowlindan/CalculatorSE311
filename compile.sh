be3ca26f6a92a1448ae7fd2fbbe1ead24544b474
Dan Dowlin <djd439@drexel.edu>
2026-03-07 02:03:36 -0500

    Basic project structure

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command fails
+set -e
+set -x
+
+# ---- Configuration ----
+SRC_DIR="src/main"
+OUT_DIR="out"
+
+# ---- Compile ----
+
+echo "Setting up output directory..."
+rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR"
+
+echo "Compiling Java sources..."
+javac -d "$OUT_DIR" $(find "$SRC_DIR" -name "*.java")
+jar cfve CalculatorServer.jar edu.drexel.se311.calculator.CalculatorClient -C out .
+jar cfve CalculatorServer.jar edu.drexel.se311.calculator.CalculatorServer -C out .
+
